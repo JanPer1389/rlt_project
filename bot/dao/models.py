b20d96c8b2e13c8c6a9842dd994c1c85e5c07d0e
JanPer1389 <josefvorobyev133@gmail.com>
2026-02-04 00:44:14 +0300

    little fix

--- bot/dao/models.py
+++ bot/dao/models.py
@@ -1,54 +1,54 @@
 from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import BigInteger, Text, ForeignKey, Date, DateTime
+from sqlalchemy import BigInteger, Text, ForeignKey
 from bot.dao.database import Base
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     
-    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
-    id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False, autoincrement=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)  
     username: Mapped[str] = mapped_column(Text, nullable=False)
     first_name: Mapped[str] = mapped_column(Text, nullable=False)
     
     def __repr__(self):
-        return f'Added User with name: <{self.first_name}; username <{self.username}>'
-    __tablename__ = "videos"
+        return f'Added User with name: <{self.first_name}>; username <{self.username}>'
 
 
 class Videos(Base):
     __tablename__ = 'videos'
-    id: Mapped[str] = mapped_column(Text)
-    creator_id: Mapped[str] = mapped_column(Text)
-    video_created_at: Mapped[str] = mapped_column(BigInteger)
-    views_count: Mapped[int] = mapped_column(BigInteger)
-    likes_count: Mapped[int] = mapped_column(BigInteger)
-    comments_count: Mapped[int] = mapped_column(BigInteger)
-    reports_count: Mapped[int] = mapped_column(Text)
+    
+    id: Mapped[str] = mapped_column(Text, primary_key=True, autoincrement=False) 
+    creator_id: Mapped[str] = mapped_column(Text, index=True, nullable=False)
+    video_created_at: Mapped[str] = mapped_column(Text)
+    views_count: Mapped[str] = mapped_column(Text)
+    likes_count: Mapped[str] = mapped_column(Text)
+    comments_count: Mapped[str] = mapped_column(Text)
+    reports_count: Mapped[str] = mapped_column(Text)
     created_at: Mapped[str] = mapped_column(Text)
     updated_at: Mapped[str] = mapped_column(Text)
     video_snap: Mapped['Video_Snapshots'] = relationship(
         'Video_Snapshots',
-        back_populates='video_snap',
+        back_populates='video',
         uselist=False,
         cascade='all, delete-orphan'
     )
 
 
 class Video_Snapshots(Base):
-    __tablename__ = 'video_snap'
+    __tablename__ = 'video_snapshots'
     
-    id: Mapped[Optional[str]] = mapped_column(Text)
-    video_id:Mapped[Optional[str]] = mapped_column(Text)
-    views_count: Mapped[Optional[int]] = mapped_column(BigInteger)
-    likes_count:Mapped[Optional[int]] = mapped_column(BigInteger)
-    reports_count:Mapped[Optional[int]] = mapped_column(BigInteger)
-    comments_count:Mapped[Optional[int]] = mapped_column(BigInteger)
-    delta_views_count: Mapped[Optional[int]] = mapped_column(BigInteger)
-    delta_likes_count: Mapped[Optional[int]] = mapped_column(BigInteger)
-    delta_comments_count: Mapped[Optional[int]] = mapped_column(BigInteger)
-    delta_reports_count:Mapped[Optional[int]] = mapped_column(BigInteger)
-    created_at: Mapped[Optional[str]] = mapped_column(Text)
-    updated_at: Mapped[Optional[str]] = mapped_column(Text)
-    videos: Mapped['Videos'] = relationship('Videos', back_populates='videos', uselist=False, cascade='all, delete-orhan')
+    id: Mapped[str] = mapped_column(Text, primary_key=True, autoincrement=False)  
+    video_id: Mapped[str] = mapped_column(ForeignKey('videos.id'), nullable=False)
+    views_count: Mapped[str] = mapped_column(Text)
+    likes_count: Mapped[str] = mapped_column(Text)
+    comments_count: Mapped[str] = mapped_column(Text)
+    reports_count: Mapped[str] = mapped_column(Text)
+    delta_views_count: Mapped[str] = mapped_column(Text)
+    delta_likes_count: Mapped[str] = mapped_column(Text)
+    delta_comments_count: Mapped[str] = mapped_column(Text)
+    delta_reports_count: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[str] = mapped_column(Text)
+    updated_at: Mapped[str] = mapped_column(Text)
+    video: Mapped['Videos'] = relationship('Videos', back_populates='video_snap')
